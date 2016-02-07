eddba65df735798c774307b7783a804026a062b2
Simone Esposito <Simone Esposito@Simone>
2016-02-07 13:14:24 +0100

    [v0.1.1 first release] upload framework
    Cambiato namespace alle classi Clause, ci saranno quelle di default e quelle del driver che possono 'overridarle'
    La classe database/query si occuperà di capire quale classe restituire

---
+++ smn/pheeca/kernel/Database/Clause/From.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace smn\pheeca\kernel\Database\Clause\Mysql;
+
+use \smn\pheeca\kernel\Database\Clause;
+
+/**
+ * Description of Select
+ *
+ * @author Simone Esposito
+ */
+class From extends Clause {
+
+    protected $_name = 'from';
+    protected $_clause = 'FROM';
+    
+
+    public function __construct($fields, $prefix = '', $suffix = '') {
+        parent::__construct([
+            'prefix' => $prefix,
+            'data' => $fields,
+            'suffix' => $suffix]
+        );
+    }
+
+    public function processFields() {
+        $fields = array();
+        $tables = $this->getData();
+        foreach ($tables as $tableAlias => $tableName) {
+            if ($tableName instanceof \Query) { // aggiustare qui !!
+                $fields[] = sprintf('(%s)', trim($tableName->toString()));
+            } else if (is_numeric($tableAlias)) {
+                $fields[] = $tableName;
+            } else {
+                $fields[] = $tableName . ' ' . $tableAlias;
+            }
+        }
+        $this->_fields = implode(', ', $fields);
+    }
+
+    public function formatString() {
+        $this->_formedString = sprintf('%s %s %s %s', $this->_clause, $this->_prefix, $this->_fields, $this->_suffix);
+    }
+
+}
