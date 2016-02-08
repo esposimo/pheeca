0d908838e502c3f8d5745729ebbfb62594173357
Simone Esposito <Simone Esposito@Simone>
2016-02-08 21:42:52 +0100

    [v0.1.1 first release] upload framework
    Implementato metodo getBindParams() per clausola From con query nidificata

--- smn/pheeca/kernel/Database/Clause/From.php
+++ smn/pheeca/kernel/Database/Clause/From.php
@@ -4,13 +4,14 @@ namespace smn\pheeca\kernel\Database\Clause;
 
 use \smn\pheeca\kernel\Database\Clause;
 use \smn\pheeca\kernel\Database\Query;
+use \smn\pheeca\kernel\Database\BindableClauseInterface;
 
 /**
  * Description of Select
  *
  * @author Simone Esposito
  */
-class From extends Clause {
+class From extends Clause implements BindableClauseInterface {
 
     protected $_name = 'from';
     protected $_clause = 'FROM';
@@ -42,5 +43,15 @@ class From extends Clause {
     public function formatString() {
         $this->_formedString = sprintf('%s %s %s %s', $this->_clause, $this->_prefix, $this->_fields, $this->_suffix);
     }
-    
+
+    public function getBindParams() {
+        $params = array();
+        foreach($this->getData() as $clause) {
+            if (($clause instanceof Query) || ($clause instanceof BindableClauseInterface)) {
+                $params = array_merge($params, $clause->getBindParams());
+            }
+        }
+        return $params;
+    }
+
 }
