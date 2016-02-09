ab630b0e277904995cb12e44183a0e8dc9ff7e5c
Simone Esposito <Simone Esposito@Simone>
2016-02-09 21:22:20 +0100

    [v0.1.1 first release] upload framework
    + Creata nuova classe clausola Call, per le chiamate a procedure. Tale classe è solo per l'adapter Mysql

---
+++ smn/pheeca/kernel/Database/Clause/Mysql/Call.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace smn\pheeca\kernel\Database\Clause\Mysql;
+
+use \smn\pheeca\kernel\Database\Clause;
+use \smn\pheeca\kernel\Database\BindableClauseInterface;
+use \smn\pheeca\kernel\Database\BindableCallInterface;
+use \smn\pheeca\kernel\Database;
+
+/**
+ * Description of Call
+ *
+ * @author Simone Esposito
+ */
+class Call extends Clause implements BindableClauseInterface {
+
+    protected $_name = 'call';
+    protected $_clause = 'CALL';
+    protected $_bind_params = array();
+
+    public function __construct($procedure_name, $bind_parameters = null, $return_parameters = null, $suffix = '', $prefix = '') {
+        parent::__construct([
+            'prefix' => $prefix,
+            'data' => $procedure_name,
+            'suffix' => $suffix]
+        );
+
+        $this->_bind_params = $bind_parameters;
+    }
+
+    public function processFields() {
+        $parameters = implode(',', $this->_bind_params);
+        $this->_fields = sprintf('%s(%s)',$this->getData(), $parameters);
+    }
+    
+    public function getBindParams() {
+        return $this->_bind_params;
+    }
+}
